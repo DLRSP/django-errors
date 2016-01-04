6be135bc2465962698e4d2698d681391925478ba
dlrsp-dev <dlrsp.dev@gmail.com>
2016-01-04 09:48:08 +0100

    add languages

--- example/example/urls.py
+++ example/example/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-	
-    # url(r'^$', include('django_errors.urls')),
+    url(r'', include('django_errors.urls')),
+    url(r'^admin/', admin.site.urls),	
 ]
