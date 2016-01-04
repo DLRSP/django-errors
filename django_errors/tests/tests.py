90b54628ebd3098f658987d7be2555ca200a4156
dlrsp-dev <dlrsp.dev@gmail.com>
2016-01-04 18:50:40 +0100

    fix test

--- django_errors/tests/tests.py
+++ django_errors/tests/tests.py
@@ -25,19 +25,19 @@ class ExampleTestCase(TestCase):
         """Test the url of 400 page."""
         LOGGER.debug("400 Test Redirect URLs")
         response = self.client.get('/400/', follow=True)
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(400, response.status_code)
 
     def test_403_urls(self):
         """Test the url of 403 page."""
         LOGGER.debug("403 Test Redirect URLs")
         response = self.client.get('/403/', follow=True)
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(403, response.status_code)
 
     def test_404_urls(self):
         """Test the url of 404 page."""
         LOGGER.debug("404 Test URLs")
         response = self.client.get('/404/', follow=True)
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(404, response.status_code)
 
     def test_404_urls(self):
         """Test that redirects kicking in when trying to go to 404 page."""
@@ -49,7 +49,7 @@ class ExampleTestCase(TestCase):
         """Test the url of 500 page."""
         LOGGER.debug("500 Test Redirect URLs")
         response = self.client.get('/500/', follow=True)
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(500, response.status_code)
 
     # def test_X_redirect_urls(self):
         # """Test that redirects end urls"""
