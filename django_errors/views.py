b13f2c4a733fda820fb1395c295697c2c2ab426d
dlrsp-dev <dlrsp.dev@gmail.com>
2016-01-04 18:06:42 +0100

    change vs render_to_response to manage code

--- django_errors/views.py
+++ django_errors/views.py
@@ -1,6 +1,7 @@
 """Django Views for django-errors module"""
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.views.generic import TemplateView
+from django.template import RequestContext
 
 class CustomView_IndexErrors(TemplateView):
     """Public Index Page. url: /"""
@@ -9,18 +10,36 @@ class CustomView_IndexErrors(TemplateView):
 def custom_400(request):
     """Custom Page for 400 error (Bad Request). url: /500"""
     error_msg = "Bad Request"
-    return render(request, 'django_errors/400.html', {'error_msg': error_msg})
-
+    response = render_to_response('django_errors/400.html',
+							{'messages': error_msg},
+							context_instance=RequestContext(request))
+    response.status_code = 400
+    return response
+	
 def custom_403(request):
     """Custom Page for 403 error (Permission Denied). url: /403"""
-    return render(request, 'django_errors/403.html')
-
+    error_msg = "Permission Denied"
+    response = render_to_response('django_errors/403.html',
+							{'messages': error_msg},
+							context_instance=RequestContext(request))
+    response.status_code = 403
+    return response
+	
 def custom_404(request):
     """Custom Page for 404 error (Not Found). url: /404"""
-    return render(request, 'django_errors/404.html')
+    error_msg = "Page Not Found"
+    response = render_to_response('django_errors/404.html',
+							{'messages': error_msg},
+							context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
 	
 def custom_500(request):
     """Custom Page for 500 error (Internal Server Errors). url: /500"""
     error_msg = "System Error"
-    return render(request, 'django_errors/500.html', {'error_msg': error_msg})
+    response = render_to_response('django_errors/500.html',
+							{'messages': error_msg},
+							context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
 
