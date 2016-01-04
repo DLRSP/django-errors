b16832f20315068776d8066d8b55ac53d9fd91c1
KingDade <davide.larosa.88@gmail.com>
2016-01-04 09:46:24 +0100

    manage urls

--- django_errors/urls.py
+++ django_errors/urls.py
@@ -10,21 +10,21 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', never_cache(views.CustomView_IndexErrors.as_view()),name="all_errors"),
-	url(r'^403/$', views.custom_403, name="403"),
-	url(r'^404/$', views.custom_404, name="404"),
-	url(r'^400/$', never_cache(views.custom_400), name="400"),
-	url(r'^500/$', never_cache(views.custom_500), name="500"),
+	url(r'^403', views.custom_403, name="403"),
+	url(r'^404', views.custom_404, name="404"),
+	url(r'^400', never_cache(views.custom_400), name="400"),
+	url(r'^500', never_cache(views.custom_500), name="500"),
 ]
 
 handler400 = views.custom_400
-""" Handle 400 also with DEBUG = True """
+""" Handle 400 error """
 
 handler403 = views.custom_403
-""" Handle 403 also with DEBUG = True """
+""" Handle 403 error """
 
-handler404 = views.custom_403
-""" Handle 404 also with DEBUG = True """
+handler404 = views.custom_404
+""" Handle 404 error """
 
 handler500 = views.custom_500
-""" Handle 500 also with DEBUG = True """
+""" Handle 500 error """
 
