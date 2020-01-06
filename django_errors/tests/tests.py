39a6450c7535184aff8e58815fffeb81009e139a
dlrsp-dev <dlrsp.dev@gmail.com>
2020-01-06 17:54:13 +0100

    - change mail - test debug response

--- django_errors/tests/tests.py
+++ django_errors/tests/tests.py
@@ -25,30 +25,35 @@ class ExampleTestCase(TestCase):
         """Test the url of 400 page."""
         LOGGER.debug("400 Test Redirect URLs")
         response = self.client.get('/400/', follow=True)
+        LOGGER.debug(response)
         self.assertEqual(400, response.status_code)
 
     def test_403_urls(self):
         """Test the url of 403 page."""
         LOGGER.debug("403 Test Redirect URLs")
         response = self.client.get('/403/', follow=True)
+        LOGGER.debug(response)
         self.assertEqual(403, response.status_code)
 
     def test_404_urls(self):
         """Test the url of 404 page."""
         LOGGER.debug("404 Test URLs")
         response = self.client.get('/404/', follow=True)
+        LOGGER.debug(response)
         self.assertEqual(404, response.status_code)
 
     def test_not_exist_urls(self):
         """Test that redirects kicking in when trying to go to 404 page."""
         LOGGER.debug("404 Test Redirect URLs")
         response = self.client.get('/UrlShouldNotExist/', follow=True)
+        LOGGER.debug(response)
         self.assertEqual(404, response.status_code)
 
     def test_500_urls(self):
         """Test the url of 500 page."""
         LOGGER.debug("500 Test Redirect URLs")
         response = self.client.get('/500/', follow=True)
+        LOGGER.debug(response)
         self.assertEqual(500, response.status_code)
 
     # def test_X_redirect_urls(self):
