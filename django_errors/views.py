a158b6ea4403011874e2d9dfc7193b7d7680b14c
dlrsp-dev <dlrsp.dev@gmail.com>
2020-01-06 19:17:18 +0100

    - clean code - add require_http_methods - use standard django.http response

--- django_errors/views.py
+++ django_errors/views.py
@@ -1,8 +1,8 @@
 """Django Views for django-errors module"""
-from django.http import HttpResponse, HttpResponseBadRequest
-from django.template import loader, RequestContext
-from django.shortcuts import render, render_to_response
+from django.http import HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotFound, HttpResponseServerError
+from django.template import loader
 from django.views.generic import TemplateView
+from django.views.decorators.http import require_http_methods
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -11,49 +11,37 @@ class CustomViewIndexErrors(TemplateView):
     template_name = "django_errors/index.html"
 
 
+@require_http_methods(["GET"])
 def custom_400(request, exception=None):
     """Custom Page for 400 error (Bad Request). url: /400"""
     error_msg = _("Bad Request")
-    response = render_to_response('django_errors/400.html', {'messages': error_msg})
-    response.status_code = 400
-    return response
+    t = loader.get_template('django_errors/400.html')
+    c = {'messages': error_msg, 'exception': exception}
+    return HttpResponseBadRequest(t.render(c, request))
 
 
-# def custom_400(request, exception=None):
-#     """Custom Page for 400 error (Bad Request). url: /500"""
-#     error_msg = _("Bad Request")
-#     response = render(request, 'django_errors/400.html', {'messages': error_msg})
-#     response.status_code = 400
-#     return response
-
-
-# def custom_400(request, exception=None):
-#     """Custom Page for 400 error (Bad Request). url: /400"""
-#     error_msg = _("Bad Request")
-#     t = loader.get_template('django_errors/400.html')
-#     c = {'messages': error_msg, 'exception': exception}
-#     return HttpResponseBadRequest(t.render(c, request))
-
-
+@require_http_methods(["GET"])
 def custom_403(request, exception=None):
     """Custom Page for 403 error (Permission Denied). url: /403"""
     error_msg = _("Permission Denied")
-    response = render(request, 'django_errors/403.html', {'messages': error_msg})
-    response.status_code = 403
-    return response
+    t = loader.get_template('django_errors/403.html')
+    c = {'messages': error_msg, 'exception': exception}
+    return HttpResponseForbidden(t.render(c, request))
 
 
+@require_http_methods(["GET"])
 def custom_404(request, exception=None):
     """Custom Page for 404 error (Not Found). url: /404"""
     error_msg = _("Page Not Found")
-    response = render(request, 'django_errors/404.html', {'messages': error_msg})
-    response.status_code = 404
-    return response
+    t = loader.get_template('django_errors/404.html')
+    c = {'messages': error_msg, 'exception': exception}
+    return HttpResponseNotFound(t.render(c, request))
 
 
+@require_http_methods(["GET"])
 def custom_500(request):
     """Custom Page for 500 error (Internal Server Errors). url: /500"""
     error_msg = _("System Error")
-    response = render(request, 'django_errors/500.html', {'messages': error_msg})
-    response.status_code = 500
-    return response
+    t = loader.get_template('django_errors/500.html')
+    c = {'messages': error_msg}
+    return HttpResponseServerError(t.render(c, request))
