f713389c92ade74d6b64abd4e71ef2a89d4100f4
dlrsp-dev <dlrsp.dev@gmail.com>
2021-01-17 10:23:14 +0100

    - fix "long_description_content_type" for pypi deploy - fix "secure" token/password for travis/pypi deploy - fix yml warning/changes

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     author_email='dlrsp.dev@gmail.com',
     packages=['django_errors', ],
     long_description=readme,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     zip_safe=False,
     install_requires=['django_nose',
