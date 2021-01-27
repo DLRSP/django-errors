c260343f3adb140eb6cfa60337ab0bbc1fa67147
dlrsp-dev <dlrsp.dev@gmail.com>
2021-01-27 16:01:20 +0100

    - update setup syntax and requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
 from setuptools import setup
-
-try:
-    readme = open('README.md').read()
-except:
-    readme = u"Wrapper Views for common errors"
-
 from django_errors import __version__ as version
+
+with open('requirements.txt') as fd:
+    requirements = [line.strip() for line in fd if line.strip()]
+
+testing_requirements = [
+    'codecov',
+    'django_nose',
+]
+
+linting_requirements = [
+    'flake8',
+    'pylint',
+    'bandit<1.7',
+]
+
+with open('README.md') as fd:
+    long_description = fd.read()
+
+if 'a' in version:
+    dev_status = '3 - Alpha'
+elif 'b' in version:
+    dev_status = '4 - Beta'
+else:
+    dev_status = '5 - Production/Stable'
 
 setup(
     name="django_errors",
@@ -16,27 +34,32 @@ setup(
     author='DLRSP',
     author_email='dlrsp.dev@gmail.com',
     packages=['django_errors', ],
-    long_description=readme,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     include_package_data=True,
     zip_safe=False,
-    install_requires=['django_nose',
-                      'django<3'],
+    data_files=[('', ['requirements.txt'])],
+    install_requires=requirements,
+    tests_require=testing_requirements,
+    extras_require={
+        'testing': testing_requirements,
+        'linting': linting_requirements,
+    },
     keywords='python django errors',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        f'Development Status :: {dev_status}',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules :: Internet :: WWW/HTTP',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Software Development :: Version Control :: Git :: Libraries :: Python Modules :: Internet :: WWW/HTTP',
     ]
 )
