ddd10809b38e1eb5d9b6a5190b18b33074a3b093
dlrsp-dev <dlrsp.dev@gmail.com>
2021-11-10 21:40:02 +0100

    - split views from urls

--- tests/urls.py
+++ tests/urls.py
@@ -1,20 +1,11 @@
 """Test's urls view for django-errors"""
-from django.http import HttpResponseNotFound, HttpResponseBadRequest, HttpResponseForbidden,\
-    HttpResponseNotAllowed, HttpResponseServerError, HttpResponse
 from django.urls import path
-
-
-def test_view(request):
-    """Test's view"""
-    if request.method != "GET":
-        return HttpResponseNotFound("404 view")
-    return HttpResponse("Test view")
-
+from views import *
 
 urlpatterns = [
     path("", test_view),
-    path("test-400/", HttpResponseBadRequest("400 view")),
-    path("test-403/", HttpResponseForbidden("403 view")),
-    path("test-405/", HttpResponseNotAllowed("POST", "405 view")),
-    path("test-500/", HttpResponseServerError("500 view")),
+    path("test-400/", test_view_400),
+    path("test-403/", test_view_403),
+    path("test-405/", test_view_405),
+    path("test-500/", test_view_500),
 ]
---
+++ tests/views.py
@@ -0,0 +1,30 @@
+"""Test's view for django-errors"""
+from django.http import HttpResponseNotFound, HttpResponseBadRequest, HttpResponseForbidden, \
+    HttpResponseNotAllowed, HttpResponseServerError, HttpResponse
+
+
+def test_view(request):
+    """Test's view"""
+    if request.method != "GET":
+        return HttpResponseNotFound("404 view")
+    return HttpResponse("Test view")
+
+
+def test_view_400():
+    """Test's view code 400"""
+    return HttpResponseBadRequest("400 view")
+
+
+def test_view_403():
+    """Test's view code 403"""
+    return HttpResponseForbidden("403 view")
+
+
+def test_view_405():
+    """Test's view code 405"""
+    return HttpResponseNotAllowed("405 view")
+
+
+def test_view_500():
+    """Test's view code 400"""
+    return HttpResponseServerError("500 view")
