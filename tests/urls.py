2785761d8c5f951f22916151a8736540da47e590
dlrsp-dev <dlrsp.dev@gmail.com>
2021-11-10 21:46:05 +0100

    - fix missing modules

--- tests/urls.py
+++ tests/urls.py
@@ -1,11 +1,11 @@
 """Test's urls view for django-errors"""
 from django.urls import path
-from views import *
+from . import views
 
 urlpatterns = [
-    path("", test_view),
-    path("test-400/", test_view_400),
-    path("test-403/", test_view_403),
-    path("test-405/", test_view_405),
-    path("test-500/", test_view_500),
+    path("", views.test_view),
+    path("test-400/", views.test_view_400),
+    path("test-403/", views.test_view_403),
+    path("test-405/", views.test_view_405),
+    path("test-500/", views.test_view_500),
 ]
