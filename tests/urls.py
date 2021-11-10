7e0ee997ac43f5d883ae838c44fbca472ecc0a0d
dlrsp-dev <dlrsp.dev@gmail.com>
2021-11-10 18:04:58 +0100

    - update tests

--- tests/urls.py
+++ tests/urls.py
@@ -1,26 +1,28 @@
-from django.http import Http404, HttpResponse
+from django.http import HttpResponseNotFound, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotAllowed, HttpResponse
 from django.urls import path
 
 
 def test_view(request):
     if request.method != "GET":
-        raise Http404()
+        return HttpResponseNotFound("Test 404 view")
     return HttpResponse("Test view")
 
 
-def test_view_http401(request):
-    return HttpResponse("Unauthorized", status=401)
+def test_view_http400(request):
+    return HttpResponseBadRequest("Test 400 view")
 
 
-def test_view_that_deletes_is_enabled(request):
-    if hasattr(request, "_cors_enabled"):
-        del request._cors_enabled
-    return HttpResponse()
+def test_view_http403(request):
+    return HttpResponseForbidden("Test 403 view")
+
+
+def test_view_http405(request):
+    return HttpResponseNotAllowed("POST", "Test 405 view")
 
 
 urlpatterns = [
     path("", test_view),
-    path("foo/", test_view),
-    path("test-401/", test_view_http401),
-    path("delete-is-enabled/", test_view_that_deletes_is_enabled),
+    path("test-400/", test_view_http400),
+    path("test-403/", test_view_http403),
+    path("test-405/", test_view_http403),
 ]
